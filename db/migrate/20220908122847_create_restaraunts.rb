d7a51799203d35e5f4c52e7bc646ca08f0b9a6e1
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-08 15:30:59 +0300

    create tebles

--- db/migrate/20220908122847_create_restaraunts.rb
+++ db/migrate/20220908122847_create_restaraunts.rb
@@ -1,4 +1,11 @@
 class CreateRestaraunts < ActiveRecord::Migration[6.1]
   def change
+    create_table :restaraunts do |t|
+      t.string :image_url
+      t.string :name
+      t.string :location
+      t.integer :phone_number
+      t.string :description
+    end
   end
 end
--- db/migrate/20220908122908_create_reviews.rb
+++ db/migrate/20220908122908_create_reviews.rb
@@ -1,4 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :restaraunt_id
+      t.string :comment
+     
+    end
   end
 end
