92b169c58dd37bc71c34473258c5df07ee54a752
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-08 15:32:59 +0300

    models

---
+++ db/schema.rb
@@ -0,0 +1,35 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_09_08_122908) do
+
+  create_table "restaraunts", force: :cascade do |t|
+    t.string "image_url"
+    t.string "name"
+    t.string "location"
+    t.integer "phone_number"
+    t.string "description"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaraunt_id"
+    t.string "comment"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
